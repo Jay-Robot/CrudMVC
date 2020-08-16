b9a284647b2bb7ad61ce81b9f9eae2f21820bd78
Jay-Robot <jota.ribeiro.silva@gmail.com>
2020-08-16 19:04:44 -0300

    Criação Migrations e scripts sql

--- src/Data/Context/DataContext.cs
+++ src/Data/Context/DataContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Data.Context
@@ -16,5 +17,27 @@ namespace Data.Context
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
         public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /* Caso alguma propriedade do tipo string não tenha sido declarada como varchar, faz isso automaticamente */
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e=> e.GetProperties().Where(p=> p.ClrType == typeof(string))))
+            {
+                property.SetColumnType("varchar(100)");
+            }
+            
+            /* Busca as entidades que estão mapeadas no contexto e busca classes que herdam de IEntityTypeConfiguration 
+               para as entidades relacionadas no contexto e registra todas de uma vez só - via reflection     */
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+
+            /* Desabilitando a deleção de vários entidades relacionadas */
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            base.OnModelCreating(modelBuilder); 
+        }
+
     }
 }
