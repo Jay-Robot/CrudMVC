5abfd4aa9c099f92380ee45f8cfaa6a1cd8827ed
Jay-Robot <jota.ribeiro.silva@gmail.com>
2020-08-16 18:24:25 -0300

    primeiro commit, criação do projeto

---
+++ src/Data/Context/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
